60c70dce30cfbf539aa37c940fd17778d1acfddb
jaekyeongg <jaekyeong@postech.ac.kr>
2023-05-26 09:04:18 +0000

    add SE_SA, CBAM

--- vgg-cifar/vgg.py
+++ vgg-cifar/vgg.py
@@ -188,6 +188,64 @@ class SpatialGate(nn.Module):
         scale = F.sigmoid(x_out) # broadcasting
         return x * scale
 
+class Flatten(nn.Module):
+    def forward(self, x):
+        return x.view(x.size(0), -1)
+    
+class ChannelGate(nn.Module):
+    def __init__(self, gate_channels, reduction_ratio=16, pool_types=['avg', 'max']):
+        super(ChannelGate, self).__init__()
+        self.gate_channels = gate_channels
+        self.mlp = nn.Sequential(
+            Flatten(),
+            nn.Linear(gate_channels, gate_channels // reduction_ratio),
+            nn.ReLU(),
+            nn.Linear(gate_channels // reduction_ratio, gate_channels)
+            )
+        self.pool_types = pool_types
+    def forward(self, x):
+        channel_att_sum = None
+        for pool_type in self.pool_types:
+            if pool_type=='avg':
+                avg_pool = F.avg_pool2d( x, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
+                channel_att_raw = self.mlp( avg_pool )
+            elif pool_type=='max':
+                max_pool = F.max_pool2d( x, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
+                channel_att_raw = self.mlp( max_pool )
+            elif pool_type=='lp':
+                lp_pool = F.lp_pool2d( x, 2, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
+                channel_att_raw = self.mlp( lp_pool )
+            elif pool_type=='lse':
+                # LSE pool only
+                lse_pool = logsumexp_2d(x)
+                channel_att_raw = self.mlp( lse_pool )
+
+            if channel_att_sum is None:
+                channel_att_sum = channel_att_raw
+            else:
+                channel_att_sum = channel_att_sum + channel_att_raw
+
+        scale = F.sigmoid( channel_att_sum ).unsqueeze(2).unsqueeze(3).expand_as(x)
+        return x * scale
+
+def logsumexp_2d(tensor):
+    tensor_flatten = tensor.view(tensor.size(0), tensor.size(1), -1)
+    s, _ = torch.max(tensor_flatten, dim=2, keepdim=True)
+    outputs = s + (tensor_flatten - s).exp().sum(dim=2, keepdim=True).log()
+    return outputs
+
+class CBAM(nn.Module):
+    def __init__(self, gate_channels, reduction_ratio=16, pool_types=['avg', 'max'], no_spatial=False):
+        super(CBAM, self).__init__()
+        self.ChannelGate = ChannelGate(gate_channels, reduction_ratio, pool_types)
+        self.no_spatial=no_spatial
+        if not no_spatial:
+            self.SpatialGate = SpatialGate()
+    def forward(self, x):
+        x_out = self.ChannelGate(x)
+        if not self.no_spatial:
+            x_out = self.SpatialGate(x_out)
+        return x_out
 
 class NewBlock(nn.Module):
     def __init__(self, channel, reduction=8):
@@ -265,6 +323,8 @@ def make_layers(cfg, batch_norm=True):
             layers += [AACN_Layer(in_channels=in_channels, image_size=img_size)]
         elif v == 'SA':
             layers += [SpatialGate()]
+        elif v == 'CBAM':
+            layers += [CBAM(in_channels,16)]
         elif v == 'NEW' :
             reduction=8
             layers += [NewBlock(in_channels, reduction)]
@@ -320,6 +380,14 @@ cfg = {
             512, 512, 512, 512, 'M'],
     'AA_345': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'AA', 'M', 512, 512, 512, 512, 'AA', 'M',
             512, 512, 512, 512, 'SE', 'M'],
+
+    # SE+SA
+    'SE_SA_12': [64, 64, 'SE', 'SA', 'M', 128, 128, 'SE', 'SA','M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M',
+        512, 512, 512, 512, 'M'],
+    
+    #CBAM
+    'CBAM_12': [64, 64, 'CBAM', 'M', 128, 128, 'CBAM', 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M',
+        512, 512, 512, 512, 'M'],
 
 }
 
